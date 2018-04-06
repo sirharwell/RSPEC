a7ba414e915e67513219cf1de34ee56049dcaa06
Ian Harwell <sirharwell@gmail.com>
2018-04-06 10:42:14 -0600

    rspec

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  validates_presence_of :first_name, :last_name, :gender, :age
+  validates_inclusion_of :gender, in: %w(Male Female Other)
+  validates :age, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 120 }
+
+  has_many :bank_accounts, dependent: :destroy
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root 'bank_accounts#index'
   devise_for :users
+  resources :bank_accounts
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
